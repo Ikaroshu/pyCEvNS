c905ced218b190a49475e46afeeb8d4fac4ead2b
Shu <xx9liao@gmail.com>
2018-08-30 17:38:47 -0500

    writing a helper linear interpolate class that should perform nicer than sicpy.interpolation.interp1d

--- pyCEvNS/flux.py
+++ pyCEvNS/flux.py
@@ -5,6 +5,7 @@ flux related class and functions
 
 from scipy.integrate import quad
 
+from .helper import *
 from .oscillation import *
 
 
@@ -44,27 +45,27 @@ class Flux:
             b8 = genfromtxt(pkg_resources.resource_filename(__name__, 'data/b8.csv'), delimiter=',')
             self.__b8x = b8[:, 0]
             self.__b8y = b8[:, 1] * ((100 * meter_by_mev) ** 2) * 5.58e6
-            self.__b8interp = interp1d(self.__b8x, self.__b8y)
+            self.__b8interp = linear_interp(self.__b8x, self.__b8y)
             f17 = genfromtxt(pkg_resources.resource_filename(__name__, 'data/f17.csv'), delimiter=',')
             self.__f17x = f17[:, 0]
             self.__f17y = f17[:, 1] * ((100 * meter_by_mev) ** 2) * 5.52e6
-            self.__f17interp = interp1d(self.__f17x, self.__f17y)
+            self.__f17interp = linear_interp(self.__f17x, self.__f17y)
             hep = genfromtxt(pkg_resources.resource_filename(__name__, 'data/hep.csv'), delimiter=',')
             self.__hepx = hep[:, 0]
             self.__hepy = hep[:, 1] * ((100 * meter_by_mev) ** 2) * 8.04e3
-            self.__hepinterp = interp1d(self.__hepx, self.__hepy)
+            self.__hepinterp = linear_interp(self.__hepx, self.__hepy)
             n13 = genfromtxt(pkg_resources.resource_filename(__name__, 'data/n13.csv'), delimiter=',')
             self.__n13x = n13[:, 0]
             self.__n13y = n13[:, 1] * ((100 * meter_by_mev) ** 2) * 2.96e8
-            self.__n13interp = interp1d(self.__n13x, self.__n13y)
+            self.__n13interp = linear_interp(self.__n13x, self.__n13y)
             o15 = genfromtxt(pkg_resources.resource_filename(__name__, 'data/o15.csv'), delimiter=',')
             self.__o15x = o15[:, 0]
             self.__o15y = o15[:, 1] * ((100 * meter_by_mev) ** 2) * 2.23e8
-            self.__o15interp = interp1d(self.__o15x, self.__o15y)
+            self.__o15interp = linear_interp(self.__o15x, self.__o15y)
             pp = genfromtxt(pkg_resources.resource_filename(__name__, 'data/pp.csv'), delimiter=',')
             self.__ppx = pp[:, 0]
             self.__ppy = pp[:, 1] * ((100 * meter_by_mev) ** 2) * 5.98e10
-            self.__ppinterp = interp1d(self.__ppx, self.__ppy)
+            self.__ppinterp = linear_interp(self.__ppx, self.__ppy)
             self.evMin = 0.003464
             self.evMax = 20
         elif not fl_file:
@@ -75,9 +76,9 @@ class Flux:
             self.__nue = f[:, 1] * ((100 * meter_by_mev) ** 2)
             self.__num = f[:, 2] * ((100 * meter_by_mev) ** 2)
             self.__nut = f[:, 3] * ((100 * meter_by_mev) ** 2)
-            self.__nueinterp = interp1d(self.__ev, self.__nue)
-            self.__numinterp = interp1d(self.__ev, self.__num)
-            self.__nutinterp = interp1d(self.__ev, self.__nut)
+            self.__nueinterp = linear_interp(self.__ev, self.__nue)
+            self.__numinterp = linear_interp(self.__ev, self.__num)
+            self.__nutinterp = linear_interp(self.__ev, self.__nut)
         else:
             raise Exception("No such flux in code or file yet.")
 
@@ -138,12 +139,12 @@ class Flux:
             return 0
         elif self.fl_type == 'solar':
             res = 0
-            res += self.__b8interp(ev)[()] if self.__b8x[0] <= ev <= self.__b8x[self.__b8x.shape[0] - 1] else 0
-            res += self.__f17interp(ev)[()] if self.__f17x[0] <= ev <= self.__f17x[self.__f17x.shape[0] - 1] else 0
-            res += self.__hepinterp(ev)[()] if self.__hepx[0] <= ev <= self.__hepx[self.__hepx.shape[0] - 1] else 0
-            res += self.__n13interp(ev)[()] if self.__n13x[0] <= ev <= self.__n13x[self.__n13x.shape[0] - 1] else 0
-            res += self.__o15interp(ev)[()] if self.__o15x[0] <= ev <= self.__o15x[self.__o15x.shape[0] - 1] else 0
-            res += self.__ppinterp(ev)[()] if self.__ppx[0] <= ev <= self.__ppx[self.__ppx.shape[0] - 1] else 0
+            res += self.__b8interp(ev) if self.__b8x[0] <= ev <= self.__b8x[self.__b8x.shape[0] - 1] else 0
+            res += self.__f17interp(ev) if self.__f17x[0] <= ev <= self.__f17x[self.__f17x.shape[0] - 1] else 0
+            res += self.__hepinterp(ev) if self.__hepx[0] <= ev <= self.__hepx[self.__hepx.shape[0] - 1] else 0
+            res += self.__n13interp(ev) if self.__n13x[0] <= ev <= self.__n13x[self.__n13x.shape[0] - 1] else 0
+            res += self.__o15interp(ev) if self.__o15x[0] <= ev <= self.__o15x[self.__o15x.shape[0] - 1] else 0
+            res += self.__ppinterp(ev) if self.__ppx[0] <= ev <= self.__ppx[self.__ppx.shape[0] - 1] else 0
             if flavor == 'e':
                 res *= survp(ev, r, epsi, 0, op)
             elif flavor == 'mu':
@@ -155,11 +156,11 @@ class Flux:
             return res
         else:
             if flavor == 'e':
-                return self.__nueinterp(ev)[()]
+                return self.__nueinterp(ev)
             elif flavor == 'mu':
-                return self.__numinterp(ev)[()]
+                return self.__numinterp(ev)
             elif flavor == 'tau':
-                return self.__nutinterp(ev)[()]
+                return self.__nutinterp(ev)
             else:
                 raise Exception('No such neutrino flavor!')
 
---
+++ pyCEvNS/helper.py
@@ -0,0 +1,26 @@
+"""
+some helper classes and functions
+"""
+
+
+from numpy import *
+
+
+class linear_interp:
+    """
+    linear interpolation that deals with numbers
+    for better performance than interp1d
+    """
+    def __init__(self, x: ndarray, y: ndarray):
+        self.x = x.copy()
+        self.y = y.copy()
+        ind = argsort(self.x)
+        self.x = self.x[ind]
+        self.y = self.y[ind]
+
+    def __call__(self, xv):
+        if xv <= self.x[0] or xv > self.x[-1]:
+            return 0
+        ind = searchsorted(self.x, xv)
+        slope = (self.y[ind] - self.y[ind-1]) / (self.x[ind] - self.x[ind-1])
+        return slope * (xv - self.x[ind-1]) + self.y[ind-1]
